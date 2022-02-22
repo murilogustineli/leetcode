b7206130024115f9f7acf996c635b1f2a3d874f3
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-02-22 17:23:43 -0500

    Add files via upload

---
+++ Programming-Solutions/58-Lenght-of-Last-Word-EASY.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/length-of-last-word/
+"""
+
+
+def lengthOfLastWord(s: str) -> int:
+    # Split string by blank spaces and reverse string
+    split = s.split(' ')[::-1]
+
+    # Iterate over the string list
+    for i in range(len(split)):
+        # Keep iterative over blank space until find a word
+        if len(split[i]) < 1:
+            continue
+        # Return length of the word
+        else:
+            return len(split[i])
+
+
+# Main function with test cases
+def main():
+    strings = [
+        'Hello World',
+        "   fly me   to   the moon  ",
+        "luffy is still joyboy"
+        ]
+    for s in strings:
+        print(lengthOfLastWord(s))
+
+
+# Call main function
+if __name__ == '__main__':
+    main()
