6229a011f8b0691846f20903b20b1f6ab394bd21
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-03-17 22:53:23 -0400

    Add files via upload

---
+++ Programming-Solutions/1476-Subrectangle-Queries-MEDIUM.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/problems/subrectangle-queries/
+"""
+
+
+class SubrectangleQueries:
+    def __init__(self, rectangle):
+        self.rectangle = rectangle
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for i in range(row1, row2 + 1):
+            for j in range(col1, col2 + 1):
+                self.rectangle[i][j] = newValue
+
+    def getValue(self, row: int, col: int) -> int:
+        return self.rectangle[row][col]
+
+
+def main():
+    global obj
+    test_cases = [
+        (["SubrectangleQueries", "getValue", "updateSubrectangle", "getValue", "getValue", "updateSubrectangle",
+          "getValue", "getValue"],
+         [[[1, 2, 1], [4, 3, 4], [3, 2, 1], [1, 1, 1]],
+          [0, 2],
+          [0, 0, 3, 2, 5],
+          [0, 2],
+          [3, 1],
+          [3, 0, 3, 2, 10],
+          [3, 1], [0, 2]])
+    ]
+    output = []
+    for array in test_cases:
+        for string in range(len(array[0])):
+            if array[0][string] == "SubrectangleQueries":
+                rectangle = array[1][0]
+                obj = SubrectangleQueries(rectangle)
+                output.append('null')
+            elif array[0][string] == "getValue":
+                row = array[1][string][0]
+                col = array[1][string][1]
+                output.append(obj.getValue(row, col))
+                # print(f"getValue: {obj.getValue(row, col)}")
+            else:
+                row1 = array[1][string][0]
+                col1 = array[1][string][1]
+                row2 = array[1][string][2]
+                col2 = array[1][string][3]
+                newValue = array[1][string][4]
+                obj.updateSubrectangle(row1, col1, row2, col2, newValue)
+                output.append('null')
+                # print(f'update: {obj.updateSubrectangle(row1, col1, row2, col2, newValue)}')
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
+
+# Your SubrectangleQueries object will be instantiated and called as such:
+# obj = SubrectangleQueries(rectangle)
+# obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+# param_2 = obj.getValue(row,col)
