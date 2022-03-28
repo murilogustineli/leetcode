f4c9d9ff2572674ed0a4801eed2dcadbd07e21fe
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-03-28 13:37:27 -0400

    Update 1886-Determine-Whether-Matrix-Can-Be-Obtained-By-Rotation-EASY.py

--- Programming-Solutions/1886-Determine-Whether-Matrix-Can-Be-Obtained-By-Rotation-EASY.py
+++ Programming-Solutions/1886-Determine-Whether-Matrix-Can-Be-Obtained-By-Rotation-EASY.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotati
 """
 
 
+# Using for loops to rotate the matrix
 def findRotation(matrix, target):
     n = len(matrix[0])
     for _ in range(4):
@@ -15,6 +16,16 @@ def findRotation(matrix, target):
     return False
 
 
+# Using zip() to rotate the matrix
+def find_rotation_simple(matrix, target):
+    for _ in range(4):
+        if matrix == target:
+            return True
+        else:
+            matrix = [list(a) for a in zip(*matrix[::-1])]
+    return False
+
+
 def main():
     test_cases = [
         ([[0, 1], [1, 0]], [[1, 0], [0, 1]]),
@@ -23,6 +34,7 @@ def main():
     ]
     for matrix, target in test_cases:
         print(findRotation(matrix, target))
+        # print(find_rotation_simple(matrix, target))
 
 
 if __name__ == '__main__':
