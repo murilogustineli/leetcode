f90b6041eb9ad5806c779264790f1792713f21fc
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-03-29 22:18:32 -0400

    Add files via upload

--- Programming-Solutions/128-Longest-Consecutive-Sequence-MEDIUM.py
+++ Programming-Solutions/128-Longest-Consecutive-Sequence-MEDIUM.py
@@ -3,7 +3,10 @@ https://leetcode.com/problems/longest-consecutive-sequence/
 """
 
 
-def longestConsecutive(nums):
+def longest_consecutive_naive(nums):
+    if len(nums) == 0:
+        return 0
+
     nums = sorted(set(nums))
     seq, temp = [], [nums[0]]
 
@@ -18,13 +21,34 @@ def longestConsecutive(nums):
     return max([len(i) for i in seq])
 
 
+def longest_consecutive(num):
+    num = set(num)
+    maxLen = 0
+    while num:
+        n = num.pop()
+        i = n+1
+        l1, l2 = 0, 0
+        while i in num:
+            num.remove(i)
+            i += 1
+            l1 += 1
+        i = n-1
+        while i in num:
+            num.remove(i)
+            i -= 1
+            l2 += 1
+        maxLen = max(maxLen, l1+l2+1)
+    return maxLen
+
+
 def main():
     test_cases = [
         [100, 101, 4, 200, 1, 3, 2],
         [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
     ]
     for nums in test_cases:
-        print(longestConsecutive(nums))
+        print(longest_consecutive_naive(nums))
+        print(longest_consecutive(nums))
 
 
 if __name__ == '__main__':
