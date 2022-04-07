0bbe037c55833351d760eefcb8032c78e8f3ab02
murilogustineli <52141042+murilogustineli@users.noreply.github.com>
2022-04-06 22:49:55 -0400

    Add files via upload

---
+++ Programming-Solutions/7-Reverse-Integer-MEDIUM.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/reverse-integer/
+"""
+
+
+def reverse(x: int) -> int:
+    # Reverse x
+    x = str(x)
+    if x[0] == '-':
+        x = int('-' + x[::-1][:-1])
+    else:
+        x = int(x[::-1])
+    # Check if x is outside the signed 32-bit integer range
+    if -2 ** 31 <= x <= 2 ** 31:
+        return x
+    else:
+        return 0
+
+
+def main():
+    test_cases = [
+        123,
+        -123,
+        120,
+        1534236469
+    ]
+    for num in test_cases:
+        print(reverse(num))
+
+
+if __name__ == '__main__':
+    main()
+
+# -2147483648 to 2147483647
